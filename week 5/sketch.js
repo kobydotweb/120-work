831218251fe346041af5116b1ace645bcaae20b5
koby <42815205+kobydotweb@users.noreply.github.com>
2018-10-17 09:51:53 -0600

    Week 5 Spinny Yin Yang

--- week 5/sketch.js
+++ week 5/sketch.js
@@ -1,10 +1,62 @@
 function setup() {
-    createCanvas( 800, 600 );
-    background( 'rgb(255, 210, 94)' );
+    createCanvas( windowWidth, windowHeight );
 }
 
+let headAngle = 0;
+let armAngle = 0;
+
+
 function draw() {
+    // erase every frame
+    background( 'pink' );
+    // turn the cursor off
     noCursor();
-    fill( 255 );
-    ellipse( mouseX, mouseY, 40 );
+
+
+    push();
+
+    translate( mouseX, mouseY );
+
+    // ARMS
+    push();
+
+
+    // arm1
+
+    push();
+    // move the center to facilitate rotate
+    translate( -10, -20 );
+    // rotate, based on mouseX position
+    rotate( radians( mouseX) );
+    // line-dot
+    line( 100, 100, 150, 100);
+    fill( 'white' );
+    noStroke();
+    ellipse( 150, 0, 50 );
+    pop();
+    pop();
+
+
+        // arm2
+
+        push();
+        // move the center to facilitate rotate
+        translate( -30, -50 );
+        // rotate, based on mouseX position
+        rotate( radians( mouseY) );
+        // line-dot
+        line( 100, 100, 150, 100);
+        fill( 'white' );
+        noStroke();
+        ellipse( 150, 0, 50 );
+        pop();
+        pop();
+
+
+
+
+
+
+    pop();
+
 }
