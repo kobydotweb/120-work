73af5d02d68ca80533b4d60bb39480dd6f1f42c8
koby <42815205+kobydotweb@users.noreply.github.com>
2018-11-18 11:16:33 -0700

    hw10

--- hw-10/sketch.js
+++ hw-10/sketch.js
@@ -1,24 +1,41 @@
 function setup() {
-	createCanvas(windowWidth, windowHeight);
-	strokeWeight(4);
+    createCanvas(windowWidth, windowHeight);
+    frameRate(1000);
 }
 
 function draw() {
-	background(`pink`);
-	for (let i = 20; i < 1000; i += 20) {
-		line(1.5*i, 0, 5*i, 1000);
-		// line(5*i, i*10, 1*i, 400);
-    line(10*i, i*10, 1*i, 10);
+    background('black');
 
-    line(1.5*i, i*3, 1/i, 800);
-    // line(3*i, 1.5*i, i, 50);
+    let num1 = floor(random(0, 200));
+    let num2 = floor(random(0, 100));
 
-    text("EEEEEEEEE", 450, 600);
-    text("EEEEEEEEE", 1300, 100);
+    let pos = addThings(num1, num2);
+
+    Shape( pos, pos, pos/300, 1.0 );
+}
 
 
+// add things
+function addThings(value1, value2) {
+    let result = value1 + value2;
+    return result;
+}
 
 
+// 	TRIANGLE FUNCTION
+function Shape(pos_x, pos_y, scale_x, scale_y) {
+    push(); // <- Begin sandbox
 
-	}
+    // scale and position triangles
+    translate(pos_x, pos_y);
+    scale(scale_x, scale_y);
+
+    // draw triangles
+		triangle(1, 100, 500, 100, mouseX, 0);
+		triangle(100, 200, 400, 200, 210, 10);
+		triangle(1000, 300, 300, 300, mouseY, 20);
+		triangle(500, 400, 200, 400, 410, 30);
+		ellipse(100, 20, 100, 200 );
+
+    pop(); // <- End sandbox
 }
