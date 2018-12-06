06f3e1576bf75366b93d41fdc7668bdbe6b4b198
koby <42815205+kobydotweb@users.noreply.github.com>
2018-12-05 18:46:52 -0700

    proposal

---
+++ hw-13/sketch.js
@@ -0,0 +1,76 @@
+let bubbles = [];
+let flower;
+let kittens = [];
+
+function preload() {
+  flower = loadImage('images/flower.png');
+  for (let i = 0; i < 3; i++) {
+    kittens[i] = loadImage(`images/png${i}.png`);
+  }
+  for (let i = 4; i < 10; i++) {
+      kittens[i] = loadImage(`images/gif${i}.gif`);
+  }
+
+
+
+
+
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  for (let i = 0; i < 10; i++) {
+    let x = random(width);
+    let y = random(height);
+    let r = random(30, 250);
+    // let kitten = random(kittens);
+    let b = new Bubble(x, y, r);
+    bubbles.push(b);
+  }
+}
+
+function mousePressed() {
+  for (let i = 0; i < bubbles.length; i++) {
+    bubbles[i].clicked(mouseX, mouseY);
+  }
+}
+
+
+function draw() {
+  background(0);
+  for (let i = 0; i < bubbles.length; i++) {
+    bubbles[i].move();
+    bubbles[i].show();
+  }
+}
+
+class Bubble {
+  constructor(x, y, r, img) {
+    this.x = x;
+    this.y = y;
+    this.r = r;
+    this.kitten = random(kittens);
+
+  }
+
+  clicked(px, py) {
+    //let d = dist(px, py, this.x, this.y);
+    //if (d < this.r) {
+    if (px > this.x && px < this.x + this.r && py > this.y && py < this.y + this.r) {
+      this.kitten = flower; //random(kittens);
+    }
+  }
+
+  move() {
+    this.x = this.x + random(-2, 2);
+    this.y = this.y + random(-2, 2);
+  }
+
+  show() {
+    image(this.kitten, this.x, this.y, this.r, this.r);
+    // stroke(255);
+    // strokeWeight(4);
+    // fill(this.brightness, 125);
+    // ellipse(this.x, this.y, this.r * 2);
+  }
+}
